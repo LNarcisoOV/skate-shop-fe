632ad3b7a605ed6e970f6f7d92464fc34bbb54d1
Leonardo Narciso Oliveira do Vale <leonardo.vale@marquise.com.br>
2019-03-27 16:58:40 -0300

    Creating buttons action.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,17 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'skateStoreApp';
+  title = 'SkateStoreApp';
   userLogin = "";
   userPassword = "";
 
+  logIn(){
+    
+  }
 
+  redirectToRegisterPage(){
+    
+  }
 }
