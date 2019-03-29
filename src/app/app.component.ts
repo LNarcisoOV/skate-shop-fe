656275a1f5fc6f2a277dd8543afb231ababea4ce
Leonardo Narciso <lnarcisoov@gmail.com>
2019-03-28 21:25:53 -0300

    Renaming project.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 
 export class AppComponent {
-  title = 'SkateStoreApp';
+  title = 'skateShopApp';
   validationMessage = '';
   login = new Login();
  
